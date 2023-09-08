9134fc2cde7c6eb097bcc5cb461008e82e95abca
majo06005 <majo06005@E-PW0234K2>
2023-09-08 14:21:59 +0200

    Add project files.

---
+++ Läxa hur gammal/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Läxa_hur_gammal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilket år föddes du?"); 
+            string årtal = Console.ReadLine();
+            int x = int.Parse(årtal);
+            int ålder = 2023 - x;
+            Console.WriteLine("Du är " + ålder + " år gammal");
+            Console.ReadKey();
+        }
+    }
+}
